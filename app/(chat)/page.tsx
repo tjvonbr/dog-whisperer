f7c72420509fcd7423f8b37e81611b41d53f80a3
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2024-05-14 20:40:23 -0700

    many changes

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,16 +1,16 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
 import { AI } from '@/lib/chat/actions'
-import { auth } from '@/auth'
+import { auth } from '@clerk/nextjs/server'
 import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
 import { redirect } from 'next/navigation'
 
 export default async function IndexPage() {
   const id = nanoid()
-  const session = (await auth()) as Session
+  const { userId } = auth()
 
-  if (!session) {
+  if (!userId) {
     redirect('/login')
   }
 
@@ -18,7 +18,7 @@ export default async function IndexPage() {
 
   return (
     <AI initialAIState={{ chatId: id, messages: [] }}>
-      <Chat id={id} session={session} missingKeys={missingKeys} />
+      <Chat id={id} userId={userId} missingKeys={missingKeys} />
     </AI>
   )
 }
