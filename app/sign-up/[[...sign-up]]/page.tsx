83ad6779ee4746eda62bbcb21244ef8b2be968a3
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2024-05-21 00:35:05 -0700

    updating ux for auth pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -16,21 +16,10 @@ export default async function SignupPage() {
   }
 
   return (
-    <div className="container grid size-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container grid size-full flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="hidden h-full bg-muted lg:block" />
-      <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <IconLogo className="mx-auto size-6" />
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Create an account
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Enter your email and password below to create your account
-            </p>
-          </div>
-          <SignUp path="/sign-up" />
-        </div>
+      <div className="h-[calc(100vh-64px)] flex flex-col justify-center items-center lg:p-8">
+        <SignUp path="/sign-up" />
       </div>
     </div>
   )
