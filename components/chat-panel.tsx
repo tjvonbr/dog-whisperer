a47e89f1961c3a71d95aca61b2c81f26f6386885
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2024-06-02 13:29:13 -0700

    enforce users through component tree

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -24,7 +24,7 @@ export interface ChatPanelProps {
   setInput: (value: string) => void
   isAtBottom: boolean
   scrollToBottom: () => void
-  user?: User | null
+  user: User
 }
 
 export function ChatPanel({
