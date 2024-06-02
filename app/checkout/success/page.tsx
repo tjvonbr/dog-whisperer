c2cc1cd67245fefc0f9060664507faacd85f626e
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2024-06-02 13:21:30 -0700

    setup stripe subscription

---
+++ app/checkout/success/page.tsx
@@ -0,0 +1,7 @@
+export default function CheckoutSuccessPage() {
+  return (
+    <div className="min-h-screen w-full flex flex-col justify-center items-center">
+      <h1>Success</h1>
+    </div>
+  )
+}
