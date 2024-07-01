c2395476783130e46d54419019f9cb3f83e27f6e
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2024-07-01 00:22:43 -0700

    update next/config file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'fs/promises': false,
+        os: false,
+        path: false
+      }
+    }
+    return config
+  },
   images: {
     domains: ['img.clerk.com'],
     remotePatterns: [
